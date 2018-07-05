ddd8f5909f22c26de4a93fc5b6a4f44fe86736f8
Dzmitry Markounikau <dzmitry.markounikau@lazada.com>
2018-07-05 15:56:04 +0700

    Added Jobs and Blog post models, added front-end for jobs homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+
+    def __str__(self):
+        if self.name:
+            return 'Post: ' + self.name
+                        
+    name = models.CharField(max_length=100, blank=True)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    pub_data = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/')
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+
+    def __str__(self):
+        if self.name:
+            return 'Job: ' + self.name
+
+    name = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
